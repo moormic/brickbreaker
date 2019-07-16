94e0608e11ffb3abb601a7f8139700eae284e363
moormic <mmoore5411@gmail.com>
2019-07-16 22:27:23 +0100

    Implement main method

--- src/main/java/com/moormic/BrickBreaker.java
+++ src/main/java/com/moormic/BrickBreaker.java
@@ -1,11 +1,31 @@
 package com.moormic;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class BrickBreaker extends JFrame {
 
-    public static void main(String[] args) {
+    private BrickBreaker() {
+        initUI();
+    }
 
+    private void initUI() {
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("BrickBreaker");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame game = new BrickBreaker();
+            game.setVisible(true);
+        });
     }
 
 }
